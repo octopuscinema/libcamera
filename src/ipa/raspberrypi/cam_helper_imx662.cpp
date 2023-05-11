c8094d7d0cf7858b1c29f27500618ee357812172
Russell Newman <russellnewman@peartree.london>
2023-05-11 23:17:37 +0100

    Added imx662 support

---
+++ src/ipa/raspberrypi/cam_helper_imx662.cpp
@@ -0,0 +1,68 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (C) 2021, Raspberry Pi (Trading) Limited
+ *
+ * added by sohonomura2020 23rd/July/2022
+ * cam_helper_imx662.cpp - camera helper for imx662 sensor
+ */
+
+#include <math.h>
+
+#include "cam_helper.hpp"
+
+using namespace RPiController;
+
+class CamHelperImx662 : public CamHelper
+{
+public:
+	CamHelperImx662();
+	uint32_t GainCode(double gain) const override;
+	double Gain(uint32_t gain_code) const override;
+	void GetDelays(int &exposure_delay, int &gain_delay,
+		       int &vblank_delay) const override;
+	unsigned int HideFramesModeSwitch() const override;
+
+private:
+	/*
+	 * Smallest difference between the frame length and integration time,
+	 * in units of lines.
+	 */
+	static constexpr int frameIntegrationDiff = 2;
+};
+
+CamHelperImx662::CamHelperImx662()
+	: CamHelper({}, frameIntegrationDiff)
+{
+}
+
+uint32_t CamHelperImx662::GainCode(double gain) const
+{
+	int code = 66.6667 * log10(gain);
+	return std::max(0, std::min(code, 0xf0));
+}
+
+double CamHelperImx662::Gain(uint32_t gain_code) const
+{
+	return pow(10, 0.015 * gain_code);
+}
+
+void CamHelperImx662::GetDelays(int &exposure_delay, int &gain_delay,
+				int &vblank_delay) const
+{
+	exposure_delay = 2;
+	gain_delay = 2;
+	vblank_delay = 2;
+}
+
+unsigned int CamHelperImx662::HideFramesModeSwitch() const
+{
+	/* After a mode switch, we seem to get 1 bad frame. */
+	return 1;
+}
+
+static CamHelper *Create()
+{
+	return new CamHelperImx662();
+}
+
+static RegisterCamHelper reg("imx662", &Create);
